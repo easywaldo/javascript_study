8d73970451935f7030340faafbe49faf38841186
easywaldoji <easywaldoji@gmail.com>
2019-09-07 17:02:27 +0900

    Generator yeild study

--- generator_ex01.js
+++ generator_ex01.js
@@ -8,3 +8,17 @@ let gen = myGenerator();
 for (let value of gen) {
     console.log(value);
 }
+
+function* myFriuts() {
+    yield "apple";
+    yield "watermelon";
+    yield "strawberry";
+    yield "grape";
+    yield "orange";
+    yield "tomato";
+    yield "banana";
+}
+
+for (let f of myFriuts()) {
+    console.log(f);
+}
